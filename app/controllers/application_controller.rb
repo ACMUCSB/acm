3c3d6a6f4e0d8823f8fe7b5b5663cbfeb1cb6db3
Andrew Berls <andrew.berls@gmail.com>
2011-11-20 16:35:18 -0800

    Basic functional user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   private
   
   def current_user
-    @current_user ||= User.find(session(:user_id)) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 end
